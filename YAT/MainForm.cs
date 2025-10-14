acf51e9a6a768f056a046641caaec47c5e321cbc
jewe <jewe@raditeq.com>
2025-10-14 14:01:04 +0200

    Adding support for 9600 baud.

--- YAT/MainForm.cs
+++ YAT/MainForm.cs
@@ -63,13 +63,13 @@ namespace YAT
             this.Text = System.Windows.Forms.Application.ProductName + " " + System.Windows.Forms.Application.ProductVersion;
 
             cboBaudRate.Items.Clear();
-
+            cboBaudRate.Items.Add(9600);
             cboBaudRate.Items.Add(19200);
             cboBaudRate.Items.Add(38400);
             cboBaudRate.Items.Add(57600);
             cboBaudRate.Items.Add(115200);
             cboBaudRate.Items.Add(921600);
-            cboBaudRate.SelectedIndex = 3;
+            cboBaudRate.SelectedIndex = 4;
 
             //set the callback
             m_serialDataReceivedEventHandler = new SerialDataReceivedEventHandler(dataReceived);
